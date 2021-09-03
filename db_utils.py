197d975a3f012d8c12e40bf8fe80d4eea85eb2b7
HendoGit <alexhenderson270@hotmail.com>
2021-09-03 16:33:08 +0100

    Testing new features

--- db_utils.py
+++ db_utils.py
@@ -7,5 +7,16 @@ with open('credentials.json') as f:
 
 end_point = 'alex-aicore.c36j3dhgfor3.eu-west-2.rds.amazonaws.com'
 port = '5432'
-db_identifier = 'alex-aicore'
-engine = create_engine(f'postgresql://postgres:{credentials["password"]}@{end_point}:{port}/{db_identifier}')
+db_identifier = 'postgres'
+engine = create_engine(f'postgresql://postgres:{credentials["password"]}@{end_point}/{db_identifier}').connect()
+
+# from sqlalchemy.sql import text
+#
+# with engine.connect() as con:
+#   file = open("query.sql")
+#   query = text(file.read())
+#
+#   con.execute(query)
+
+
+league_table.to_sql(con=database_connection, name="LEAGUE_TABLE", if_exists='replace',chunksize=100, index=False)
